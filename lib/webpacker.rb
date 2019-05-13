40d9738c3756c9cb310b80e3e605547700449f5d
willnet <netwillnet@gmail.com>
2019-05-13 11:29:01 +0900

    require "active_support/core_ext/delegation" is needed explicitly

--- lib/webpacker.rb
+++ lib/webpacker.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/module/attribute_accessors"
 require "active_support/core_ext/string/inquiry"
 require "active_support/core_ext/logger"
 require "active_support/tagged_logging"
+require "active_support/core_ext/delegation"
 
 module Webpacker
   extend self
